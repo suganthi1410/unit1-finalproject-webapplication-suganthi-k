7cba76023bb0a45c9561e10f0d5376b5609941a7
sugi1410 <suganthi1410@gmail.com>
2026-07-07 14:01:15 -0500

    modified the Header.jsx links to all pages added in the header

--- unit1-finalproject-suganthik/src/components/layout/Header.jsx
+++ unit1-finalproject-suganthik/src/components/layout/Header.jsx
@@ -17,11 +17,20 @@ function Header(){
 <img src={logo} alt="Nirtya LOGO" width="100" />
 <h1 >Nirtya by Teacher's Name</h1>
 
-            </div>
-
-        
-<nav>
-
+            </div>        
+<nav
+style={{
+    display:"flex",
+    justifyContent:"flex-end",
+    alignItems:"center",
+    gap: "30px"
+}}
+>
+<Link to="/">Home</Link>
+<Link to="/about">About</Link>
+<Link to="/class">Class</Link>
+<Link to="/gallery">Gallery</Link>
+<Link to="/contact">Contact</Link>
 </nav>
 
     </header>

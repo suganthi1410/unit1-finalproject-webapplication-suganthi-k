cdf29674c45abdd7e48a3d5d26fb2f8a4ab81cc6
sugi1410 <suganthi1410@gmail.com>
2026-07-07 14:04:14 -0500

    modified App.jsx,added import,added routepath

--- unit1-finalproject-suganthik/src/App.jsx
+++ unit1-finalproject-suganthik/src/App.jsx
@@ -2,6 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/layout/Header.jsx';
 
+import Home from './components/pages/Home.jsx';
+import About from './components/pages/About.jsx';
+import Class from './components/pages/Class.jsx';
+import Gallery from './components/pages/Gallery.jsx';
+import Contact from './components/pages/Contact.jsx';
+
+
 import ClassSchedule from './components/classschedule/ClassSchedule.jsx';
 import ClassTimings from "./components/classschedule/ClassTimings";
 
@@ -12,9 +19,16 @@ function App() {
     <Header />
 
       <Routes>
-        <Route path="/" element={<ClassSchedule />} />
-        <Route path="/schedule" element={<ClassSchedule />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/class" element={<Class />} />
+        <Route path="/schedule/timings" element={<ClassTimings />} />  
+        {/*
+       <Route path="/schedule" element={<ClassSchedule />} />
         <Route path="/schedule/timings" element={<ClassTimings />} />
+        */} 
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );

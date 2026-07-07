2ce1566adfb49789b632f2ebfdfbc49f666c3583
sugi1410 <suganthi1410@gmail.com>
2026-07-07 13:59:46 -0500

    created components for all pages,import,export done

---
+++ unit1-finalproject-suganthik/src/components/pages/Class.jsx
@@ -0,0 +1,11 @@
+import ClassSchedule from "../classschedule/ClassSchedule";
+
+function Class(){
+    return(
+        <div>
+            <h2>Class Schedule</h2>
+            <ClassSchedule />
+        </div>
+    );
+};
+export default Class;
---
+++ unit1-finalproject-suganthik/src/components/pages/Gallery.jsx
@@ -0,0 +1,9 @@
+function Gallery(){
+    return(
+        <div>
+            <h1>Gallery page in progress</h1>
+        </div>
+    );
+
+    };
+    export default Gallery;

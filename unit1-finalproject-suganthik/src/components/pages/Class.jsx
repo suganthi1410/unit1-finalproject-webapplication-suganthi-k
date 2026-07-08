74c5263e4624d92fede9ab5e1f9db39b6252d09e
sugi1410 <suganthi1410@gmail.com>
2026-07-08 15:58:46 -0500

    removed unwanted line from the class.jsx

--- unit1-finalproject-suganthik/src/components/pages/Class.jsx
+++ unit1-finalproject-suganthik/src/components/pages/Class.jsx
@@ -1,9 +1,9 @@
 import ClassSchedule from "../classschedule/ClassSchedule";
 
+
 function Class(){
     return(
         <div>
-            <h2>Class Schedule</h2>
             <ClassSchedule />
         </div>
     );

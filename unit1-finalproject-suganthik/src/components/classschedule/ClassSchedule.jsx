e82de46e9d9ede42e1dee89b1f7f74520aa28e29
sugi1410 <suganthi1410@gmail.com>
2026-07-08 15:56:41 -0500

    Refactor ClassSchedule to use StudentType state, conditionally render KidsLevels/AdultsLevels also the link is there for class timings

--- unit1-finalproject-suganthik/src/components/classschedule/ClassSchedule.jsx
+++ unit1-finalproject-suganthik/src/components/classschedule/ClassSchedule.jsx
@@ -1,35 +1,37 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import KidsLevels from '../classschedule/KidsLevels'
+import AdultsLevels from '../classschedule/AdultLevels'
+
 
 function ClassSchedule(){
+  const [StudentType, setStudentType] = useState("");
+
   return (
-    <div>
-      <h2>Class Levels</h2>
-      <p>Select a level to learn more, or view all class timings below.</p>
+    <div className="class-schedule-page">
 
-    <section>
-        <h3>Beginner Level</h3>
-        <p>Perfect for new students learning basic steps and posture.</p>
-        <p><strong>Fees:</strong> $60/month</p>
-      </section>
-
-      
-      <section>
-        <h3>Intermediate Level</h3>
-        <p>For students who know basics and want to improve technique.</p>
-        <p><strong>Fees:</strong> $75/month</p>
-      </section>
-
-      
-      <section>
-        <h3>Advanced Level</h3>
-        <p>For experienced dancers focusing on performance and choreography.</p>
-        <p><strong>Fees:</strong> $90/month</p>
-      </section>
-
-      
+      <h2>CLASS LEVELS AND TIMINGS</h2>
+      <h3>Levels</h3>
+      <div className ="button-type-toggle">
+        <button 
+        onClick={() => setStudentType("Kids")} >
+          Kids
+                  </button>
+                  <button
+                    onClick={() => setStudentType("Adults")} >
+                      Adults
+                  </button>                
+                 
+      </div>
+       <div className="levels-container">
+                    {StudentType === "Kids" && <KidsLevels /> }
+                    {StudentType === "Adults" && <AdultsLevels />}
+                  </div>     
+      <div className= "timings container">
       <h3>
         <Link to="/schedule/timings">View Class Timings</Link>
       </h3>
+    </div>
     </div>
   );
 }

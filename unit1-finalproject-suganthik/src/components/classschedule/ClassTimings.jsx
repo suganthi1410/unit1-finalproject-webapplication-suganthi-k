c0c543fdfd9af19fcb78819873ce9a7aefe96ab2
sugi1410 <suganthi1410@gmail.com>
2026-07-09 15:08:02 -0500

    Refractored the ClassTimings.jsx(major/parent component) that works well-when i selected the type of student and level it shows the relevent day,time and fees

--- unit1-finalproject-suganthik/src/components/classschedule/ClassTimings.jsx
+++ unit1-finalproject-suganthik/src/components/classschedule/ClassTimings.jsx
@@ -1,26 +1,46 @@
-import timings from './mockdata/TimeData.js';
+import { useState } from 'react';
+import timings from './mockdata/ClassTimingsData.js';
+import TimingsDisplay from './TimingsDisplay.jsx';
 
 function ClassTimings() {
-  const kids = timings.filter(item => item.group.startsWith("Kids"));
-  const adults = timings.filter(item => item.group.startsWith("Adults"));
 
+  const [selectedType, setSelectedType] = useState("");
+    const [selectedLevel, setSelectedLevel ] = useState("");
+
+    const filteredTimings = timings.filter((timingInfo) =>{
+      if(!selectedType || !selectedLevel) return false;
+
+      return(
+        timingInfo.type === selectedType &&
+      timingInfo.level === selectedLevel
+    );
+  });
+    console.log("selectedType:",selectedType );
+    console.log("selectedLevel:",selectedLevel);
+    console.log("Filtered:", filteredTimings);
+
+    
   return (
     <div>
-      <h2>Class Timings</h2>
+      <h3>ClassTimings for Selected Level </h3>
 
-      <h3>Kids</h3>
-      {kids.map((item, index) => (
-        <p key={index}>
-          {item.group}: {item.time}
-        </p>
-      ))}
+      <select value={selectedType} onChange= {(e) =>
+      setSelectedType(e.target.value)} >
+        <option value="">Select Type </option>
+        <option value="Kids">Kids </option>
+        <option value="Adults">Adults</option>        
+      </select>
 
-      <h3>Adults</h3>
-      {adults.map((item, index) => (
-        <p key={index}>
-          {item.group}:{item.time}
-        </p>
-      ))}
+      <select value = {selectedLevel} onChange={(e) =>
+      setSelectedLevel(e.target.value)} >
+        <option value="">Select Level </option>
+        <option value="Beginner">Beginner</option>
+        <option value="Intermediate">Intermediate</option>
+        <option value="Advanced">Advanced</option>
+
+      </select>
+      <TimingsDisplay items={filteredTimings} />
+    
     </div>
   );
 }

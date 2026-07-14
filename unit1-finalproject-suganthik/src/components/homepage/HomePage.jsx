dee09de082e0555eca47a671aa25dcf9c922952b
sugi1410 <suganthi1410@gmail.com>
2026-07-14 00:09:38 -0500

    new bannerimage added to the assets/images, refactored the HomePage.jsx and css added to the homepage,work in progress need to add button to the class page

--- unit1-finalproject-suganthik/src/components/homepage/HomePage.jsx
+++ unit1-finalproject-suganthik/src/components/homepage/HomePage.jsx
@@ -1,31 +1,29 @@
 import { Link } from 'react-router-dom';
-import banner from '../../assets/images/banner.jpeg'
+import banner from '../../assets/images/bannerbw1.jpeg'
 import './HomePage.css';
 
 function HomePage() {
     return (
         <div className="home-page">
-            <section className="banner">
+              <section className="banner">
                 <img src={banner} alt="Dancer and Natarajar" className="banner-image" />
                 <div className="banner-text">
-                    <h4>Embrace the Rhythm and Experience the grace and power of Bharathanatym</h4>
+                    <h4>Welcome to Nirtya - Embrace the rhythm and Experience the grace and power of Bharathanatym</h4>
                 </div>
             </section>
             <section className="about-bharatham">
                 <h3>About Bharathanatyam</h3>
-                <p>
-                    Bharatanatyam is one of India's oldest classical dance forms, known for
+                <p>Bharatanatyam is one of India's oldest classical dance forms, known for
                     its elegance, precision, and expressive storytelling.
                     Bharatanatyam has three core elements:
                     Nritta : Pure dance movements focused on rhythm and technique, without storytelling.
                     Nritya : Expressive dance using facial expressions, gestures, and body language to convey emotion.
                     Natya  : Dramatic storytelling that combines movement, expression, and narrative.
                 </p>
-                <h2>Welcome</h2>
-                <p>
-                    Welcome to Nritya by ___name___ , a space where tradition meets grace.
-                    We celebrate the beauty of Bharatanatyam, guiding every student to
-                    grow with confidence, devotion, and joy.
+                <h2>Dance for everyone</h2>
+                <p>From curious kids to passionate adults, everyone is welcome to learn, grow, and express through Bharatanatyam.
+                    </p>
+                    <p className="quote">Your age doesn't define your path — your passion does.
                 </p>
             </section>
 

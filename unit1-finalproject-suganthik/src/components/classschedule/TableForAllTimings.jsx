52fd45b464a672aa386f1b1813ad734552c3c881
sugi1410 <suganthi1410@gmail.com>
2026-07-15 11:17:03 -0500

    function for table created using mock data

--- unit1-finalproject-suganthik/src/components/classschedule/TableForAllTimings.jsx
+++ unit1-finalproject-suganthik/src/components/classschedule/TableForAllTimings.jsx
@@ -3,6 +3,31 @@ import timings from './mockdata/ClassTimingsData.js';
 function TableForAllTimings(){
     return(
         <div>
+            <h3>Class Timings for Kids and Adults</h3>
+            <table>
+                <thead>
+                    <tr> 
+                        <th>Type</th>
+                        <th> Level</th>
+                        <th> Day</th>
+                        <th>Time</th>
+                        <th>fee</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+{timings.map((item, index) =>(
+   <tr key={index}>
+    <td>{item.type}</td>
+    <td>{item.level}</td>
+    <td>{item.Day}</td>
+    <td>{item.time}</td>
+    <td>{item.fee}</td>
+    </tr>
+))}
+
+                </tbody>
+            </table>
 
         </div>
     );

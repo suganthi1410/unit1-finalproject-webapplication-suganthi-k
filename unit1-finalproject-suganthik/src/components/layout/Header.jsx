096cf7d046a193170b93a48eb5badd63a6d77e3e
sugi1410 <suganthi1410@gmail.com>
2026-07-15 15:35:23 -0500

    inline css have been deleted from the header.jsx and logo image were moved from the assets to public,steps included in text file

--- unit1-finalproject-suganthik/src/components/layout/Header.jsx
+++ unit1-finalproject-suganthik/src/components/layout/Header.jsx
@@ -2,30 +2,13 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.jpeg';
 function Header(){
     return(
-        <header
-         style={{
-        padding: "5px",
-        borderBottom: "6px solid #d22828"
-      }}
-         >
-            <div
-             style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "15px" }}
-            >
+        <header className="header">
+            <div className="logo-title">
 <img src={logo} alt="Nirtya LOGO" width="100" />
-<h1 >Nirtya by Teacher's Name</h1>
-
-            </div>        
-<nav
-style={{
-    display:"flex",
-    justifyContent:"flex-end",
-    alignItems:"center",
-    gap: "30px"
-}}
->
+<h1 className="title">Nirtya by Bhavithra Ganesh</h1>
+            </div>       
+             
+<nav className="header-nav" >
 <Link to="/">Home</Link>
 <Link to="/about">About</Link>
 <Link to="/class">Class</Link>

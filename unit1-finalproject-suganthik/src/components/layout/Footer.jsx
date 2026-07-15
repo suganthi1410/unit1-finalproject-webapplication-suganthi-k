0720e9e71de94fd7e2dc4beb1798c41b30856805
sugi1410 <suganthi1410@gmail.com>
2026-07-15 18:33:32 -0500

    image added to public that is used for footer,refactored Footer.jsx;

--- unit1-finalproject-suganthik/src/components/layout/Footer.jsx
+++ unit1-finalproject-suganthik/src/components/layout/Footer.jsx
@@ -1,11 +1,32 @@
+import { Link } from'react-router-dom';
+import footerimage from '../../../public/footerimage1.jpeg';
 function Footer(){
     const year = new Date().getFullYear();
-    return(
-        <div>
+    return(        
         <footer>
-            <p>&copy;{ year } Nirtya by Teachers name copyrights reserved.</p>
+            <div>
+            <p>&copy;{ year } Nirtya by Bhavithra Ganesh.All rights reserved.</p>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/class">Class</Link>
+                <Link to="/gallery">Gallery</Link>
+                <Link to="/contact">Contact</Link>
+            
+            <a
+                href="https://www.youtube.com/@bharathanatyam9772/featured"
+                target="_blank" >
+                    Youtube
+            </a>
+            </nav>
+            <div >
+                <img src={footerimage} alt="Dancer BW" width="100" />
+            </div>
+            
+                </div>
+            
         </footer>
-        </div>
+      
     );
 }
 export default Footer;

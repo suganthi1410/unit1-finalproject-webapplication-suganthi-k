0b65872037e3cb87b3e6dc8198c0149946382968
sugi1410 <suganthi1410@gmail.com>
2026-07-16 11:17:48 -0500

    added state(reviews and text) added input (wrting review)

--- unit1-finalproject-suganthik/src/components/gallerypage/Reviews.jsx
+++ unit1-finalproject-suganthik/src/components/gallerypage/Reviews.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 
 function Reviews(){
+const [reviews, setReviews] = useState([]);
+const [text, setText] =useState("");
+
     return(
         <div>
+            <input
+            value={text}
+            onChange={(e) => setText(e.target.value)} 
+            placeholder='Write review here'
+            />
+            
 
         </div>
     )

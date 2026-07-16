2463827ec1de71862615dea61861bff327a29d3b
sugi1410 <suganthi1410@gmail.com>
2026-07-15 20:38:33 -0500

    refactored the ooter jsx and applied css for that

--- unit1-finalproject-suganthik/src/components/layout/Footer.jsx
+++ unit1-finalproject-suganthik/src/components/layout/Footer.jsx
@@ -1,29 +1,32 @@
 import { Link } from'react-router-dom';
-import footerimage from '../../../public/footerimage1.jpeg';
+import footerimage from '../../../public/footer.jpeg';
 function Footer(){
     const year = new Date().getFullYear();
     return(        
-        <footer>
-            <div>
-            <p>&copy;{ year } Nirtya by Bhavithra Ganesh.All rights reserved.</p>
-            <nav>
+        <footer className="footer">
+            <div className="footer-left">
+                <p className="footer-title">QUICK LINKS</p>            
+            <nav className="footer-nav">
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/class">Class</Link>
                 <Link to="/gallery">Gallery</Link>
                 <Link to="/contact">Contact</Link>
-            
-            <a
-                href="https://www.youtube.com/@bharathanatyam9772/featured"
-                target="_blank" >
-                    Youtube
+                 </nav>
+            <div className="youtube-link" >
+            <a href="https://www.youtube.com/@bharathanatyam9772/featured"
+                target="_blank"  > Youtube
             </a>
-            </nav>
-            <div >
-                <img src={footerimage} alt="Dancer BW" width="100" />
             </div>
-            
-                </div>
+             <p className="copyright">
+           &copy; {year} Nirtya by Bhavithra Ganesh. All rights reserved.
+        </p>
+            </div>
+            <div className="footer-right" >
+                <img src={footerimage} alt="Natarajar BW" width="300" />
+            </div>
+
+               
             
         </footer>
       
